73bba5b9890ab3a432cad6cbfc41e01ffa5adbf9
Elias John Cajal <Elias.Cajal.d102422@mcast.edu.mt>
2020-05-03 17:29:31 +0200

    03/05/20

---
+++ HomeAssigment/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PropertyWebSolution.Models
+{
+    public class Item
+    {
+        ///<summary>
+        ///id  is the primary key.. it should be named id
+        ///
+        ///
+
+        [Key]
+        public int ItemId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "your ")]
+        public string ItemName { get; set; }
+
+
+
+
+    }
+}
---
+++ HomeAssigment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeAssigment.Startup))]
+namespace HomeAssigment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
